f34b5f981bdb5dc615809fc522206422ee425ba6
tunglam268 <73482349+tunglam268@users.noreply.github.com>
2021-08-03 15:23:26 +0700

    Add files via upload

--- controllers/usercontrollers_test.go
+++ controllers/usercontrollers_test.go
@@ -24,43 +24,43 @@ func testGetAllUser(t *testing.T) {
 		t.Errorf("handler returned unexpected bot: got %v want %v", w.Body.String(), expected)
 	}
 }
-func testGetUserById(t *testing.T){
-	req , err := http.NewRequest("GET","/get/{id}",nil)
-	if err != nil{
+func testGetUserById(t *testing.T) {
+	req, err := http.NewRequest("GET", "/get/{id}", nil)
+	if err != nil {
 		t.Fatal(err)
 	}
 	q := req.URL.Query()
-	q.Add("id","1")
+	q.Add("id", "1")
 	req.URL.RawQuery = q.Encode()
 	w := httptest.NewRecorder()
 	handler := http.HandlerFunc(GetUserByID)
 	handler.ServerHTTP(w, req)
-	if status := w.Code, status != http.StatusOK{
+	if status := w.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 
 	}
 	expected := `[{"id":1,"name":"test","balance":600000,"created_time":"12:00:00","modified_time":"12:00:00"}]`
-	if w.Body.String() != expected{
+	if w.Body.String() != expected {
 		t.Errorf("handler returned unexpected bot: got %v want %v", w.Body.String(), expected)
 	}
 }
 
-func testUpdateUserById(t *testing.T){
-	var jsonStr =[]byte(`{"id":1,"name":"test","balance":600000,"created_time":"12:00:00","modified_time":"12:00:00"}`)
-	req , err := http.NewRequest("PUT","/update.{id}",bytes.NewBuffer(jsonStr))
-	if err != nil{
+func testUpdateUserById(t *testing.T) {
+	var jsonStr = []byte(`{"id":1,"name":"test","balance":600000,"created_time":"12:00:00","modified_time":"12:00:00"}`)
+	req, err := http.NewRequest("PUT", "/update.{id}", bytes.NewBuffer(jsonStr))
+	if err != nil {
 		t.Fatal(err)
 	}
-	req.Header.Set("Content-Type","application/json")
+	req.Header.Set("Content-Type", "application/json")
 	w := httptest.NewRecorder()
 	handler := http.HandlerFunc(GetUserByID)
 	handler.ServerHTTP(w, req)
-	if status := w.Code, status != http.StatusOK{
+	if status := w.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 
 	}
 	expected := `[{"id":1,"name":"test","balance":600000,"created_time":"12:00:00","modified_time":"12:00:00"}]`
-	if w.Body.String() != expected{
+	if w.Body.String() != expected {
 		t.Errorf("handler returned unexpected bot: got %v want %v", w.Body.String(), expected)
 	}
 }
